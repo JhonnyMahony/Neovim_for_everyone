c956757319ff5c95ee11d91bd62405b067106fd1
JhonnyMahony <ralldiars@gmail.com>
2024-02-03 20:48:36 +0200

    barbar added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,6 +10,10 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+vim.keymap.set('n', '<Tab>', '<Cmd>BufferPrevious<CR>')
+vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferNext<CR>')
+vim.keymap.set('n', '<c-x>', '<Cmd>BufferClose<CR>')
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
